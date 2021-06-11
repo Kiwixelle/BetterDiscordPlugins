84ad8c39e689a127cc98c98a72b9de2daeeca86a
DarSitam <85754915+DarSitam@users.noreply.github.com>
2021-06-11 18:23:22 +0200

    Update SoundEffect.plugin.js

--- SoundEffect.plugin.js
+++ SoundEffect.plugin.js
@@ -19,8 +19,8 @@
 			}],
 			version: "1.0.1",
 			description: "plugin BetterDiscord qui joue certains sons quand certains mots sont envoyés",
-            github: "https://github.com/Metalloriff/BetterDiscordPlugins/blob/master/Bruh.plugin.js",
-		    github_raw: "https://raw.githubusercontent.com/DarSitam/BD-Sound-Effects/main/SoundEffect.plugin.js"
+            		github: "https://github.com/Metalloriff/BetterDiscordPlugins/blob/master/Bruh.plugin.js",
+		    	github_raw: "https://raw.githubusercontent.com/DarSitam/BD-Sound-Effects/main/SoundEffect.plugin.js"
 		},
         
 		defaultConfig: [{
